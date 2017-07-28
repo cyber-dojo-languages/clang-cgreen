af50318b78de2144e294285f8bb70b43b1c18b7b
Jon Jagger <jon@jaggersoft.com>
2017-07-28 11:14:51 +0530

    use red/amber/green regexs from clang++(assert)

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,8 +1,9 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  return :red   if / Failure: /.match(output)
-  return :red   if / Exception: /.match(output)
-  return :green if /0 failures, 0 exceptions/.match(output)
+  return :amber unless /^Completed/.match(output)
+  return :red   if /(\d+) failure/.match(output)
+  return :red   if /(\d+) exception/.match(output)
+  return :green if /(\d+) pass/.match(output)
   return :amber
 }
